3a845e325ce74906eda0201c32b9fea42d1e95a5
Pavel Krokhmal <pavel.krokhmal.mn@outlook.com>
2021-07-22 15:00:40 +0300

    create mock server

--- pages/posts.js
+++ pages/posts.js
@@ -1,13 +1,29 @@
+import {useEffect, useState} from 'react'
 import Head from "next/head";
 import {MainLayout} from "../components/MainLayout";
 
 export default function Posts() {
+
+    const [posts, setPosts] = useState([])
+
+    useEffect(()=>{
+        async function load() {
+            const response = await fetch('http://localhost:4200/posts')
+            const json = await response.json()
+            setPosts(json)
+        }
+        load()
+    }, [])
+
     return (
         <MainLayout title={"Posts page"}>
             <Head>
                 <title>Posts page</title>
             </Head>
             <h1>Posts</h1>
+            <pre>
+                {JSON.stringify(posts, null, 2)}
+            </pre>
         </MainLayout>
     )
 }
